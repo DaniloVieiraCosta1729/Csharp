d053a208225ae4a50411b9155f80e240d2e3f7f7
Danilo Vieria Costa <danilo.11235@outlook.com>
2023-10-15 23:35:59 -0300

    Adiciona um app do tipo console e demonstra discute variaveis.

---
+++ Testando/RelembrandoCS/RelembrandoCS/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Variables
+
+// Não inicializada
+int idade;
+idade = 44;
+// Inicializada
+string nome = "Gascoigne";
+// Várias variáveis na mesma linha
+string primeiroNome = "Maria", honorifico = "Lady";
+// Constante
+const int idadeMaria = 28;
+// Binário
+sbyte binario = 0b001101;
+// Hexadecimal
+ushort hexadecimal = 0xA2;
+// Display
+Console.WriteLine(nome+" tem "+idade+" anos.");
+Console.WriteLine($"{honorifico} {primeiroNome} tem {idadeMaria} anos.");
+Console.WriteLine($"O número binário 1101 na base dez é {binario}");
+Console.WriteLine($"O número hexadecimal A2 em decimal é {hexadecimal}");

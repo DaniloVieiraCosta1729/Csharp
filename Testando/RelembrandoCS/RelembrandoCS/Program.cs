202c7e6aea3602401d6e3a72fa042416862686f0
Danilo Vieria Costa <danilo.11235@outlook.com>
2023-10-16 07:35:55 -0300

    Explica uma diferença entre (int)d e Convert.ToInt32(d).

--- Testando/RelembrandoCS/RelembrandoCS/Program.cs
+++ Testando/RelembrandoCS/RelembrandoCS/Program.cs
@@ -27,6 +27,10 @@ float f = j;
 float g = 2.71828483f;
 int i2 = (int)g;
 short j2 = (short)i2;
+// Detalhes sobre conversão
+double d = 3.94159;
+int i3 = (int)d; // Trunca 3.94159, resultado -> 3
+int j3 = Convert.ToInt32(d); // Arredonda 3.14159 para inteiro, resultado -> 4
 
 // Display
 Console.WriteLine(nome+" tem "+idade+" anos.");
@@ -36,3 +40,4 @@ Console.WriteLine($"O número hexadecimal A2 em decimal é {hexadecimal}");
 Console.WriteLine($"==============================================================================");
 Console.WriteLine($"Byte i = {i}, int j = {j}, float = {f}");
 Console.WriteLine($"Float g = {g}, int i2 = {i2}, short j2 = {j2}");
+Console.WriteLine($"Double d = {d}, (int)d = {i3}, Convert.ToInt32(d) = {j3}.");
